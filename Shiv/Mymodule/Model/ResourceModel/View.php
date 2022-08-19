9e809625e6c1df21226051bd90e3e875a99b60e3
SHIV KUMAR JHA <72031407+SHIV167@users.noreply.github.com>
2022-08-19 16:53:38 +0530

    Add files via upload

---
+++ Shiv/Mymodule/Model/ResourceModel/View.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Shiv\Mymodule\Model\ResourceModel;
+
+use \Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
+class View extends AbstractDb
+{
+    /**
+     * Post Abstract Resource Constructor
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Shiv_article', 'article_id');
+    }
+}
---
+++ Shiv/Mymodule/Model/View.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Shiv\Mymodule\Model;
+
+use \Magento\Framework\Model\AbstractModel;
+use \Magento\Framework\DataObject\IdentityInterface;
+use \Shiv\Mymodule\Api\Data\ViewInterface;
+
+/**
+ * Class File
+ * @package Shiv\Mymodule\Model
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class View extends AbstractModel implements ViewInterface, IdentityInterface
+{
+    /**
+     * Cache tag
+     */
+    const CACHE_TAG = 'Shiv_mymodule_view';
+
+    /**
+     * Post Initialization
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Shiv\Mymodule\Model\ResourceModel\View');
+    }
+
+
+    /**
+     * Get Title
+     *
+     * @return string|null
+     */
+    public function getTitle()
+    {
+        return $this->getData(self::TITLE);
+    }
+
+    /**
+     * Get Content
+     *
+     * @return string|null
+     */
+    public function getContent()
+    {
+        return $this->getData(self::CONTENT);
+    }
+
+    /**
+     * Get Created At
+     *
+     * @return string|null
+     */
+    public function getCreatedAt()
+    {
+        return $this->getData(self::CREATED_AT);
+    }
+
+    /**
+     * Get ID
+     *
+     * @return int|null
+     */
+    public function getId()
+    {
+        return $this->getData(self::ARTICLE_ID);
+    }
+
+    /**
+     * Return identities
+     * @return string[]
+     */
+    public function getIdentities()
+    {
+        return [self::CACHE_TAG . '_' . $this->getId()];
+    }
+
+    /**
+     * Set Title
+     *
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        return $this->setData(self::TITLE, $title);
+    }
+
+    /**
+     * Set Content
+     *
+     * @param string $content
+     * @return $this
+     */
+    public function setContent($content)
+    {
+        return $this->setData(self::CONTENT, $content);
+    }
+
+    /**
+     * Set Created At
+     *
+     * @param string $createdAt
+     * @return $this
+     */
+    public function setCreatedAt($createdAt)
+    {
+        return $this->setData(self::CREATED_AT, $createdAt);
+    }
+
+    /**
+     * Set ID
+     *
+     * @param int $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        return $this->setData(self::ARTICLE_ID, $id);
+    }
+}
